7a69b5047b75f5fdee268282df2cad7fc815fe8f
bhulakshmi <damarlabhulakshmi95@gmail.com>
2019-11-05 16:48:49 +0530

    timepickerin the form

---
+++ src/components/FormFields/TimePickerinpt.js
@@ -0,0 +1,17 @@
+import { TimePicker } from 'antd';
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+
+function onChange(time, timeString) {
+  console.log(time, timeString);
+}
+class TimePickerinpt extends Component{
+    render(){
+        return(
+            <div>
+                <TimePicker className="inpt_size" placeholder={this.props.placeholder} use12Hours format="h:mm a" onChange={onChange} />
+            </div>
+        );
+    }
+}
+export default TimePickerinpt;
