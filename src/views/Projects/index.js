0555a6638cb429c6bbe42e5b1544c8221ef5d7e6
amanovishnu <amanovishnu@gmail.com>
2019-11-21 10:56:39 +0530

    Redux File Structure

--- src/views/Projects/index.js
+++ src/views/Projects/index.js
@@ -1,22 +1,6 @@
-// import React, { Component } from "react";
-
-
-// class Projects extends Component {
-//   counter = useSelector(state => state.counter);
-//   render() {
-//     return (
-//       <div>
-//         Projects
-//         <h1>Counter {counter()}</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Projects;
-import React from 'react'
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement} from '../../actions/index';
+import { increment, decrement} from '../../redux/actions/index';
 const Projects = () => {
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
