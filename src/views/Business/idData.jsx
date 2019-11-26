d1236b7da0a139c6241b79f9440ae05dfb3ca8e1
amanovishnu <amanovishnu@gmail.com>
2019-11-26 14:57:22 +0530

    UI Changes

--- src/views/Business/idData.jsx
+++ src/views/Business/idData.jsx
@@ -1,67 +1,66 @@
-let idData = {
-    clientName: 'Hyderabad Metro Rail Corperation',
-    clientEquipmentConut: 1000,
-    clientEquipmentDetails: {
-        0: {
+let idData = [
+    ['Hyderabad Metro Rail Corperation'],
+    [
+        {
             equipmentName: "Excavator",
             equipmentId: "EQ101",
             equipmentMake: "2001",
             equipmentCount: 200
         },
-        1: {
+        {
             equipmentName: "Backhoe Loader",
             equipmentId: "EQ102",
             equipmentMake: "2005",
             equipmentCount: 20
         },
-        2: {
+        {
             equipmentName: "Tipper",
             equipmentId: "EQ103",
             equipmentMake: "2009",
             equipmentCount: 157
         },
-        3: {
+        {
             equipmentName: "Transit Mixed",
             equipmentId: "EQ104",
             equipmentMake: "1997",
             equipmentCount: 500
         },
-        4: {
+        {
             equipmentName: "Tanker",
             equipmentId: "EQ105",
             equipmentMake: "2019",
             equipmentCount: 658
         },
-        5: {
+        {
             equipmentName: "Grader",
             equipmentId: "EQ106",
             equipmentMake: "2019",
             equipmentCount: 5
         },
-        6: {
+        {
             equipmentName: "Bull Dozer",
             equipmentId: "EQ107",
             equipmentMake: "2019",
             equipmentCount: 759
         },
-        7: {
+        {
             equipmentName: "Wheel Loader",
             equipmentId: "EQ108",
             equipmentMake: "2019",
             equipmentCount: 985
         },
-        8: {
+        {
             equipmentName: "Telescopic Crane",
             equipmentId: "EQ109",
             equipmentMake: "2019",
             equipmentCount: 84
         },
-        9: {
+        {
             equipmentName: "Single Drum Loader",
             equipmentId: "EQ110",
             equipmentMake: "2019",
             equipmentCount: 57
         }
-    }
-}
-export { idData }
+    ]
+]
+export { idData };
