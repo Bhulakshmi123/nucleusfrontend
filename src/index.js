a3f2a5c45f501036432ff0fb7797f319cf7ad196
amanovishnu <amanovishnu@gmail.com>
2019-11-26 17:53:40 +0530

    UI Changes and Icon Removal

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
 // import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
 // import "./assets/css/demo.css";
-import "./assets/css/pe-icon-7-stroke.css";
+// import "./assets/css/pe-icon-7-stroke.css";
 import "./assets/css/rootStyles.css";
 import AdminLayout from "layouts/Admin.jsx";
 import { createStore, applyMiddleware, compose } from 'redux';
