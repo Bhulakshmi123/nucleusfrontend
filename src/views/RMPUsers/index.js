57b3baa98b191f41b7c1ae3f733715360babc649
amanovishnu <amanovishnu@gmail.com>
2019-11-27 15:48:50 +0530

    UI Changes

--- src/views/RMPUsers/index.js
+++ src/views/RMPUsers/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+// import React from 'react';
+// import { sidebarViewAction } from '../../redux/actions';
+// import { useSelector, useDispatch } from 'react-redux';
+// const RMPUsers = () => {
+//   const sidebarView = useSelector(state => state.sidebarView);
+//   const dispatch = useDispatch();
+//   const dataFromUrl = () => { dispatch(sidebarViewAction(false)) };
+//   componentDidMount: function() {
+
+//   }
+//   return (
+//     <React.Fragment>
+//       {dataFromUrl()}
+//       <div className={sidebarView ? "mainContent mainContentMini" : "mainContent"}>
+//         <h1 className="text-white text-center mt-5">RMP Users</h1>
+//       </div>
+//     </React.Fragment>
+//   )
+// }
+// export default RMPUsers;
+import React, { Component } from 'react';
 import { sidebarViewAction } from '../../redux/actions';
 import { useSelector, useDispatch } from 'react-redux';
-const RMPUsers = () => {
-  const sidebarView = useSelector(state => state.sidebarView);
-  const dispatch = useDispatch();
-  const dataFromUrl = () => { dispatch(sidebarViewAction(false)) }
-  return (
-    <React.Fragment>
-      {dataFromUrl()}
-      <div className={sidebarView ? "mainContent mainContentMini" : "mainContent"}>
-        <h1 className="text-white text-center mt-5">RMP Users</h1>
+class RMPUsers extends Component {
+  
+  render() {
+    return (
+      <div>
+        sjalkdjkl
       </div>
-    </React.Fragment>
-  )
+    )
+  }
 }
+
 export default RMPUsers;
