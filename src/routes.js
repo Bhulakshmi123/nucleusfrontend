0405a05a1d9f7b3b3a3fc3c1b1a9ded73866cf6e
amanovishnu <amanovishnu@gmail.com>
2019-11-28 11:33:33 +0530

    Unwanted Files Removed

--- src/routes.js
+++ src/routes.js
@@ -37,7 +37,7 @@ const dashboardRoutes = [{
 },
 {
     path: "/rmp-users",
-    name: "Rmp Users",
+    name: "RMP Users",
     icon: <AiOutlineUser />,
     component: RMPUsers,
     layout: "/nucleus"
