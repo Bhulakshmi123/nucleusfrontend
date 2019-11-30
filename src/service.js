636629df950eaf059d559e7295153c5de2d20f04
bhulakshmi <damarlabhulakshmi95@gmail.com>
2019-11-30 17:19:38 +0530

    Api

--- src/service.js
+++ src/service.js
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-const env = '192.168.0.194:9018';
-
-const Service = {
-
-    fetchPost: async(url, body, token = null) => {
-        url = env + url;
-        const headers = { 'Content-Type': 'application/json' };
-        if (token) {
-            headers['x-access-token'] = token;
-            headers['source'] = 'website';
-=======
 // const env = require('./config').api_server;
 const env = 'http://127.0.0.1:9018';
 
 const Service = {
-    fetchGet: async (url, token = null) => {
+    fetchGet: async(url, token = null) => {
         url = env + url;
         const headers = {};
         headers['source'] = 'nucleus';
@@ -43,21 +31,17 @@ const Service = {
         }
     },
 
-    fetchPost: async (url, body, token = null) => {
+    fetchPost: async(url, body, token = null) => {
         url = env + url;
         const headers = { 'Content-Type': 'application/json' };
         headers['source'] = 'website';
         if (token) {
             headers['x-auth-token'] = token;
->>>>>>> 37f40e500a9b9b11453e9d09fc38eff5a0401b90
         }
         try {
             const res = await fetch(url, { method: 'POST', headers: headers, body: JSON.stringify(body) });
             if (res.status >= 200 && res.status < 400) {
-<<<<<<< HEAD
-=======
                 console.log(res);
->>>>>>> 37f40e500a9b9b11453e9d09fc38eff5a0401b90
                 return (res.json());
             } else if (res.status >= 400 && res.status < 500) {
                 console.log(res);
@@ -70,11 +54,6 @@ const Service = {
             console.log(e);
             return false;
         }
-<<<<<<< HEAD
-    }
-};
-export default Service;
-=======
     },
 
 
@@ -86,4 +65,3 @@ const onFailure = (type) => {
 };
 
 export default Service;
->>>>>>> 37f40e500a9b9b11453e9d09fc38eff5a0401b90
