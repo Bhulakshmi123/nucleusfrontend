332ecac5741d8362da7f0058bfb60ff04256d3d7
bhulakshmi <bhulakshmi@equiphunt.com>
2020-03-04 14:27:42 +0530

    #1

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 const store = createStore(allReducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 class Index extends Component {
-	render () {
+	render() {
 		return (
 			<React.Fragment>
 				<Provider store={store}>
