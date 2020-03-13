c647c7ec76f9898ff975fa87d081cf5db7bca54c
kamalgupta123 <guptakamal137@gmail.com>
2020-03-13 10:28:20 +0530

    03/13/20 10:28:20 by kamal

--- git.bash
+++ git.bash
@@ -1,5 +1,6 @@
+gitCommit = $1
 git status
 git add -A
-git commit -m "$(date +"%D %T") by $(whoami)"
+git commit -m "$(date +"%D %T") by $(whoami) $gitCommit"
 # This part is Still Under Development
 # git pull origin master
