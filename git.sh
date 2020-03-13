a1b5d9e6570c5de8ae338eb2eef4a91412e32040
kamalgupta123 <guptakamal137@gmail.com>
2020-03-13 10:37:36 +0530

    03/13/20 10:37:36 by kamal -m Start.sh & git.sh Files are Added to Ignore List

--- git.sh
+++ git.sh
@@ -1,8 +1,6 @@
 FILE1=$1
 echo $FILE1
-# wc $FILE1
 git status
 git add -A
-git commit -m "$(date +"%D %T") by $(whoami) $FILE1"
-# This part is Still Under Development
-# git pull origin master
+git commit -m "$(date +"%D %T") by $(whoami) -m $FILE1"
+git pull origin master
